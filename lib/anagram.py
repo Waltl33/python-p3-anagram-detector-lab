8471a9d3090aa60f3248e55bc4d7eaed429c9485
Walter <walter.clark24@yahoo.com>
2023-09-19 13:20:26 -0400

    second test passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,3 +1,6 @@
 class Anagram:
     def __init__(self, word):
         self.word_letters = sorted([letter for letter in word])
+        
+    def match(self, word_list):
+          match_word_list = []
